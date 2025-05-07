fb02e11a22e84c84c8aeea6770ef8179a94b5453
Gabriel Araujo <=>
2025-05-07 11:43:24 -0300

    fix: tailwindcss v4, shadcn, typescript + vite setup

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,20 +1,19 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
+    <div className="min-h-screen flex flex-col items-center justify-center p-6 bg-background text-foreground">
       <div className="flex gap-8 mb-8">
         <a
           href="https://vite.dev"
           target="_blank"
           className="hover:scale-110 transition-transform"
         >
-          <img src={viteLogo} className="w-24 h-24" alt="Vite logo" />
+          <img src={viteLogo} className="h-24 w-24" alt="Vite logo" />
         </a>
         <a
           href="https://react.dev"
@@ -23,28 +22,28 @@ function App() {
         >
           <img
             src={reactLogo}
-            className="w-24 h-24 animate-spin-slow"
+            className="h-24 w-24 animate-spin-slow"
             alt="React logo"
           />
         </a>
       </div>
-      <h1 className="text-4xl font-bold text-gray-800 mb-8">Vite + React</h1>
-      <div className="bg-white p-6 rounded-lg shadow-md max-w-md w-full mb-8">
+      <h1 className="text-4xl font-bold mb-8">Vite + React</h1>
+      <div className="bg-card text-card-foreground p-6 rounded-lg shadow-md mb-8 w-full max-w-md">
         <button
           onClick={() => setCount((count) => count + 1)}
-          className="w-full bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-md transition-colors mb-4"
+          className="w-full bg-primary hover:cursor-pointer  text-primary-foreground py-2 px-4 rounded-md mb-4 hover:bg-primary/90 transition-colors"
         >
           count is {count}
         </button>
-        <p className="text-gray-600">
+        <p className="text-muted-foreground">
           Edit{" "}
-          <code className="bg-gray-100 px-1 py-0.5 rounded text-sm font-mono">
-            src/App.jsx
+          <code className="bg-muted px-1.5 py-0.5 rounded text-sm">
+            src/App.tsx
           </code>{" "}
           and save to test HMR
         </p>
       </div>
-      <p className="text-gray-500 text-sm">
+      <p className="text-sm text-muted-foreground">
         Click on the Vite and React logos to learn more
       </p>
     </div>
